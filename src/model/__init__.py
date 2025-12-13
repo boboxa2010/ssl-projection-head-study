ad88d41533539fdee197859cc2f61207d8aaee32
Ilia1243x <ilja.indenbaum@gmail.com>
2025-12-13 13:20:57 +0300

    Add everything for their exps

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,5 +1,5 @@
 from src.model.resnet import ResNetModel, ResNetImg32
 from src.model.heads import UniversalProjectionHead
-from src.model.stacked import SimCLR
+from src.model.stacked import SimCLR, ProjectedClassifier
 
-__all__ = ["ResNetModel", "ResNetImg32", "UniversalProjectionHead", "SimCLR"]
+__all__ = ["ResNetModel", "ResNetImg32", "UniversalProjectionHead", "SimCLR", "ProjectedClassifier"]
