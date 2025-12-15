a8111edbcf3c22e425722c02e305cb21ae6f6971
Irene <nerhneiro@gmail.com>
2025-12-15 09:28:37 +0300

    add mnist-pn-cifar class

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -3,4 +3,4 @@ from src.datasets.stl import STLDataset
 from src.datasets.rotated_stl import RotatedSTLDataset
 from src.datasets.mnist import MNISTDataset
 
-__all__ = ["CIFARDataset", "STLDataset", "RotatedSTLDataset", "MNISTDataset"]
+__all__ = ["CIFARDataset", "STLDataset", "RotatedSTLDataset", "MNISTDataset", "MNISTonCIFARDataset"]
